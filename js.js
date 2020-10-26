a4a239f8a6acedeb0e0c8e674ea9835a8a726afc
Fmial <71271240+Fmial@users.noreply.github.com>
2020-10-26 15:45:01 +1000

    first commit

---
+++ js.js
@@ -0,0 +1,17 @@
+let obj = {
+    'James': ['Jacob', 'Bill', 'Lucas'],
+    'Johnny': ['David', 'Kyle', 'Lucas'],
+    'Peter': ['Lucy', 'Kyle']
+};
+
+let news = obj['James'];
+
+console.log(news);
+
+let view = ['Lucas', 'Bill'];
+
+function findout(ponoz, watchers) {
+    let exemp = ponoz
+}
+
+findout(obj, view)
